721d362cb575a063aab391e861ef263e8fac57a9
mandos21 <mt.degenaro@gmail.com>
2026-04-15 14:47:56 -0400

    fix bootstrap script

--- k3s/cluster/apps/nas-attached/music-ingest/workload/beets/config/bootstrap.sh
+++ k3s/cluster/apps/nas-attached/music-ingest/workload/beets/config/bootstrap.sh
@@ -13,27 +13,27 @@ PY
 if command -v fish >/dev/null 2>&1; then
   mkdir -p /root/.config/fish/completions /root/.config/fish/conf.d
   cat >/root/.config/fish/conf.d/beets-admin.fish <<'FISH_EOF'
-  function beet --wraps beet
-      command beet -c /state/config.yaml $argv
-  end
-  function b --wraps beet
-      command beet -c /state/config.yaml $argv
-  end
-  function bl --wraps beet
-      command beet -c /state/config.yaml ls $argv
-  end
-  function bi --wraps beet
-      command beet -c /state/config.yaml import $argv
-  end
-  function bb --wraps beet
-      command beet -c /state/config.yaml bad $argv
-  end
-  function bdup --wraps beet
-      command beet -c /state/config.yaml duplicates $argv
-  end
-  if status --is-interactive; and test "$PWD" = /
-      cd /uploads
-  end
-  FISH_EOF
+function beet --wraps beet
+    command beet -c /state/config.yaml $argv
+end
+function b --wraps beet
+    command beet -c /state/config.yaml $argv
+end
+function bl --wraps beet
+    command beet -c /state/config.yaml ls $argv
+end
+function bi --wraps beet
+    command beet -c /state/config.yaml import $argv
+end
+function bb --wraps beet
+    command beet -c /state/config.yaml bad $argv
+end
+function bdup --wraps beet
+    command beet -c /state/config.yaml duplicates $argv
+end
+if status --is-interactive; and test "$PWD" = /
+    cd /uploads
+end
+FISH_EOF
   HOME=/root beet -c /state/config.yaml fish >/dev/null 2>&1 || true
 fi
